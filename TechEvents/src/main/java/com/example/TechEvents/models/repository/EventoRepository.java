00e5eb01a5ac10faa5feb1ebcf11c21d05763446
jesusod <99189407+jesusod@users.noreply.github.com>
2022-05-09 14:31:24 +0200

    Delete TechEvents directory

--- TechEvents/src/main/java/com/example/TechEvents/models/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TechEvents.models.repository;
-
-import com.example.TechEvents.models.entity.Evento;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventoRepository extends CrudRepository<Evento, Long> {
-
-
-}
--- TechEvents/src/main/java/com/example/TechEvents/models/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TechEvents.models.repository;
-
-import com.example.TechEvents.models.entity.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-
-
-}
